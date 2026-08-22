a64b2065f6fa7ba57091595a72c161cc2f4b04d7
PedroAP14 <pamericoparussolo@gmail.com>
2026-08-21 22:53:00 -0300

    v1

---
+++ calculadoragorgeta/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace calculadoragorgeta
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
