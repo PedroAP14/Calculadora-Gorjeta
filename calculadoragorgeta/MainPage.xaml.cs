6825dab559132a60d171db2db3947b61a83dceed
PedroAP14 <pamericoparussolo@gmail.com>
2026-08-28 22:33:16 -0300

    v1.5

--- calculadoragorgeta/MainPage.xaml.cs
+++ calculadoragorgeta/MainPage.xaml.cs
@@ -2,11 +2,39 @@
 {
     public partial class MainPage : ContentPage
     {
-
+        double valorConta = 0;
         public MainPage()
         {
             InitializeComponent();
         }
 
+        private void Gorgeta15PorcentoButton_Clicked(object sender, EventArgs e)
+        {
+            PercentualGorgetaLabel.Text = "15%";
+        }
+
+        private void Gorgeta20PorcentoButton_Clicked(object sender, EventArgs e)
+        {
+            PercentualGorgetaLabel.Text = "20%";
+        }
+
+        private void ArrendondarParaBaixoButton_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BotaoArrendodarParaCimaButton_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PorcetagemValorSlider_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            PercentualGorgetaLabel.Text = PorcetagemValorSlider.Value.ToString();
+            valorConta = Convert.ToDouble(ValorContaEntry.Text);
+            double valorGorgeta = valorConta * (PorcetagemValorSlider.Value / 100);
+
+            ValorGorgetaLabel.Text = valorGorgeta.ToString(); 
+        }
     }
 }
